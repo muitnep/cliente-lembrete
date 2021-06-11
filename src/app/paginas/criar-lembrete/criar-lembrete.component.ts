20f886588bf3dd53d3a9f64e197d53d21c6db3c8
Rodrigo Santos Bonifácio Rodrigues <muitnep@gmail.com>
2021-06-11 18:08:01 -0300

    Criado a classe do nosso metodo criar-lembrete

--- src/app/paginas/criar-lembrete/criar-lembrete.component.ts
+++ src/app/paginas/criar-lembrete/criar-lembrete.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ErrorMsgComponent } from 'src/app/compartilhado/error-msg/error-msg.component';
+import { LembreteService } from 'src/app/services/lembrete.service';
+import { Router } from '@angular/router';
+import { Lembrete } from 'src/app/interfaces/lembrete';
 
 @Component({
   selector: 'app-criar-lembrete',
   templateUrl: './criar-lembrete.component.html',
   styleUrls: ['./criar-lembrete.component.css']
 })
-export class CriarLembreteComponent implements OnInit {
+export class CriarLembreteComponent {
 
-  constructor() { }
+  @ViewChild(ErrorMsgComponent) errorMsgComponent!: ErrorMsgComponent;
 
-  ngOnInit(): void {
+  constructor(private lembreteService: LembreteService, private router: Router) { }
+
+  addLembrete(lembrete: Lembrete) {
+    this.lembreteService.addLembrete(lembrete).subscribe(
+      () => { this.router.navigateByUrl('/'); },
+      () => { this.errorMsgComponent.setError('Falha ao adicionar lembrete'); });
   }
 
 }
