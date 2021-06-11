488522983303c63f3fc080a41d150749b5ea5a81
Rodrigo Santos Bonifácio Rodrigues <muitnep@gmail.com>
2021-06-11 18:17:58 -0300

    adicionado nova rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarLembreteComponent } from './paginas/criar-lembrete/criar-lembrete.component';
 import { ListaLembreteComponent } from './paginas/lista-lembrete/lista-lembrete.component';
 
 const routes: Routes = [
-  { path: '', component: ListaLembreteComponent }
+  { path: '', component: ListaLembreteComponent },
+  { path: 'lembrete/criar', component: CriarLembreteComponent }
 ];
 
 @NgModule({
