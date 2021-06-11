8e071204e5dace4338df1714561d160ae6ada236
Rodrigo Santos Bonifácio Rodrigues <muitnep@gmail.com>
2021-06-11 17:34:41 -0300

    Criado o formulario no form-lembrete.component.html

--- src/app/compartilhado/form-lembrete/form-lembrete.component.ts
+++ src/app/compartilhado/form-lembrete/form-lembrete.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Lembrete } from 'src/app/interfaces/lembrete';
 
 @Component({
   selector: 'app-form-lembrete',
   templateUrl: './form-lembrete.component.html',
   styleUrls: ['./form-lembrete.component.css']
 })
-export class FormLembreteComponent implements OnInit {
+export class FormLembreteComponent {
+  @Input() lembrete: Lembrete = <Lembrete>{};
+  @Output() outputLembrete: EventEmitter<Lembrete> = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSubmit() {
+    this.outputLembrete.emit(this.lembrete);
   }
+
+
 
 }
