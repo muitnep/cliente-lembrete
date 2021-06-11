b5cb6c67b68a6f2c2d60f8b3e83e39d0293bd58a
Rodrigo Santos Bonifácio Rodrigues <muitnep@gmail.com>
2021-06-11 17:57:56 -0300

    Criado o componente criar-lembrete

---
+++ src/app/paginas/criar-lembrete/criar-lembrete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-lembrete',
+  templateUrl: './criar-lembrete.component.html',
+  styleUrls: ['./criar-lembrete.component.css']
+})
+export class CriarLembreteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
